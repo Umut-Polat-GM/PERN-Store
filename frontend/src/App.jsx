41b980fe44afa947e390dfd39fe7c32bb0684b33
Umut-Polat-GM <umut.polat.272@gmail.com>
2025-03-03 16:25:16 +0300

    add frontend structure with initial pages, theme management, and styling

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+
+import { useThemeStore } from "./store/useThemeStore";
+
+import ProductPage from "./pages/ProductPage";
+import HomePage from "./pages/HomePage";
+
+import Navbar from "./components/Navbar";
+
+function App() {
+    const { theme } = useThemeStore();
+    return (
+        <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}>
+            <Navbar />
+
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/product/:id" element={<ProductPage />} />
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
